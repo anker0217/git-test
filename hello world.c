a37c76b2e8b33f998e0b858fd5333a117e982052
zxx <923235965@qq.com>
2025-11-10 20:22:47 +0800

    add input block

--- hello world.c
+++ hello world.c
@@ -2,13 +2,15 @@
  * @Author: zxx 923235965@qq.com
  * @Date: 2025-11-10 18:00:09
  * @LastEditors: zxx 923235965@qq.com
- * @LastEditTime: 2025-11-10 20:08:30
+ * @LastEditTime: 2025-11-10 20:22:30
  * @FilePath: \gitRepo\hello world.c
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 #include <stdio.h>
 int main()
 {
+    // input block
+    getchar();
     printf("This is branch moreif\n");
     printf("This is branch moreLoop\n");
     if (1)
